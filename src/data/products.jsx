209503ac4935a779868f00e73e641c8a95c636a2
Ansh Sharma <anshsharma2463@gmail.com>
2025-07-22 12:09:14 +0530

    correction

--- src/data/products.jsx
+++ src/data/products.jsx
@@ -131,7 +131,7 @@ export const categories = [
     id: 'vintage',
     name: 'Vintage Collection',
     description: 'Classic racing heritage meets modern style',
-    isActive: false,
+    isActive: true,
     image: 'https://www.vintagedrip.in/cdn/shop/collections/vintagerj1.png?v=1732048866&width=750'
   },
   {
